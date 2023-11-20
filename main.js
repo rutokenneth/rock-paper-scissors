b0d17a629bb5210be75214a1c4ae523a21657638
rutokenneth <rutokenneth16@gmail.com>
2023-11-20 06:42:08 +0300

    Chore: Got my the getComputerChoice function working on the console

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
-
+function getComputerChoice() {
+    // This is the computer's choice. It will be randomly generated from an array of choices:
+    const computerChoices = ["Rock", "Paper", "Scissors"];
+    const randomChoice = Math.floor(Math.random() * computerChoices.length);
+    return computerChoices[randomChoice];
+}
+let computerSelection = getComputerChoice();
+console.log(computerSelection);
