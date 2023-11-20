25b64f375f3cdce6c8b5370204e5791d2f4e727c
rutokenneth <rutokenneth16@gmail.com>
2023-11-20 07:07:38 +0300

    Chore: Play Function is working seamlessly

--- main.js
+++ main.js
@@ -5,4 +5,25 @@ function getComputerChoice() {
     return computerChoices[randomChoice];
 }
 let computerSelection = getComputerChoice();
-console.log(computerSelection);
+console.log(computerSelection)
+// The play function
+
+function play(playerSelection, computerSelection) {
+    playerSelection = playerSelection.toLowerCase();
+    computerSelection = computerSelection.toLowerCase();
+
+    if (playerSelection === computerSelection) {
+        return "It is a tie!";
+    } else if (
+        (playerSelection === "rock" && computerSelection === "scissors") ||
+        (playerSelection === "paper" && computerSelection === "rock") ||
+        (playerSelection === "scissors" && computerSelection === "paper")
+    ) {
+        return `You win! ${playerSelection} beats ${computerSelection}`;
+    } else {
+        return `You lose! ${computerSelection} beats ${playerSelection}`;
+    }
+}
+
+let round1 = play("rock", computerSelection)
+console.log(round1);
